0f6960eb62cb3f04814ce2321313f23d63645687
User19 <user19@user19deimac.local>
2019-03-14 10:36:16 +0800

    Initial Commit

---
+++ hw1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by User19 on 2019/3/14.
+//  Copyright © 2019 Starmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
